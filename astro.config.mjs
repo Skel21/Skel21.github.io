392ee193203976011981fd9093d8aa0231264745
Skel21 <bartlomiej.leks@gmail.com>
2025-12-01 14:59:30 +0100

    add tailwindcss styling add content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://skel21.github.io',
+  site: 'https://skel21.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
